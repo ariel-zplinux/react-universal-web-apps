7c46ffc923ad997aa007839a0f2fbb37836888b1
noshir <noshir@zplinux.com>
2016-12-22 17:40:23 +0200

    Aggregations managed by promises

--- app/test.js
+++ app/test.js
@@ -6,32 +6,50 @@ const FileData = require('./models/FileData.js');
 
 // Initialize database as soon as possible
 const mongoose = require('./infra/db-manager');
+
 mongoose.connection.once('connected', () => {
     // drop db to start in a clean state
     console.log('Database intialized');
     mongoose.connection.db.dropDatabase();
 
-    // Start operations
+    // ----------------------
+    // Start operations (main)
+    // ----------------------
+    let promiseIndex = 1;
+    console.log('[promise '+promiseIndex+'] Addition of nodejs event loop data');
     readNodeJsEventLoopFilePromise()
         .then((id) => {
-            console.log(id+' added (step 2)');
+            promiseIndex++;
+            console.log('[promise '+promiseIndex+'] Addition of readme data - after '+id+' added');
             return readReadMeFilePromise();
         })
         .then( () => {
-            console.log('before dictionary');
+            promiseIndex++;
+            console.log('[promise '+promiseIndex+'] Extraction of dictionary');
             return readDictionaryPromise();
         })
-        .then( (res) => {
-            console.log('dictionary added');
-            return readSessionPromise(res);
+        .then( (req) => {
+            promiseIndex++;
+            console.log('[promise '+promiseIndex+'] Extraction of session data - after '+req.previousPhase);
+            return readSessionPromise(req);
         })
-        .then( (id) => {
-            console.log(id+' added (the end)');
+        .then( (req) => {
+            promiseIndex++;
+            console.log('[promise '+promiseIndex+'] Aggregation of session data  - after '+req.previousPhase);
+            return sessionDataAggregationPromise(req);
+        })
+        .then( (req) => {  
+            promiseIndex++;          
+            console.log('[last promise] after '+req+' - the end');
             mongoose.connection.close();            
         }).catch( (err) => {
             console.log(err);
         });        
 });
+
+// --------
+// contexts
+// --------
 
 const contextNodeJsEventLoopFile = {
     id: 'nodejs_event_loop',
@@ -53,18 +71,146 @@ const contextSessionFile = {
     path: 'app/fixtures/airbnb_session_data.txt'
 };
 
-const readSessionPromise = (res) => {
+// -----------------------------------
+// Aggregation (map reduce) parameters
+// -----------------------------------
+
+const clientPerUserDeviceMapReduce = {
+    map: function() { 
+        emit(this.device, 1);
+    },
+    reduce: function(device, n) { 
+        return Array.sum(n);
+    },
+    // scope: {
+    //     total: lines_ok
+    // },
+    finalize: function(key, reduced_value) {
+        return {
+            name: key,
+            value:""+((reduced_value / total) * 100).toFixed(2)+"%" // precentage
+        };
+    },
+    out: 'clients_per_user_device'
+};
+
+const clientPerUserAgentMapReduce = {
+    map: function() { 
+        emit(this.dim_user_agent, 1)
+    },
+    reduce: function(device, n) {
+        return Array.sum(n);
+    },
+    finalize: function(key, reduced_value) {
+        return {
+            name: key,
+            value: reduced_value
+        };
+    },
+    out: 'clients_per_user_agent'
+};
+
+const UserSessionDurationMapReduce = {
+    map: function() {
+        // calculate session duration
+        var duration = new ISODate(this.ts_max) - new ISODate(this.ts_min);
+        emit(this.device, duration)
+    },
+    reduce: function(device, n) {
+        // reduce to sum of duration per device
+        return Array.sum(n);
+    },
+    finalize: function(key, reduced_value) {
+        var days = Math.floor( reduced_value / ( 3600 * 24 * 1000) );
+        days = days > 1 ? days+"d" : "";   
+        var hours = new Date(reduced_value).getHours();
+        hours = hours ? hours + "h" : "";
+        var minutes = new Date(reduced_value).getMinutes(); 
+        minutes = minutes ?  minutes+ "m" : ""; 
+        var seconds = new Date(reduced_value).getSeconds();
+        seconds = seconds ? seconds + "s" : "";
+        var duration = days + hours + minutes + seconds;
+        return {
+            name: key,
+            value: duration // _d_h_m_s format
+        };
+    },
+    out: 'duration_per_user_device'
+};
+
+
+//--------
+//promises
+//--------
+
+const sessionDataAggraegationPromise = (req) => {
+    return new Promise( (resolve, reject) => {
+        const RawData = req.model;
+
+        // remove first document that contains field names
+        RawData.find({device: "dim_device_app_combo"}).remove().exec();
+
+        // Aggregation functions to extract relevant data        
+
+        // scope added to map-reduce with value stored in a passed arguement
+        clientPerUserDeviceMapReduce.scope = {
+            total: req.lines_ok
+        };
+        RawData.mapReduce(clientPerUserDeviceMapReduce, (err, data, stats) => { 
+            if (err)
+                reject(err) 
+            else if (stats){
+                console.log('\t"Clients per device" map reduce took %d ms', stats.processtime);
+                done();
+            }
+        });
+
+        RawData.mapReduce(clientPerUserAgentMapReduce, (err, data, stats) => { 
+            if (err)
+                reject(err)
+            else if (stats){
+                console.log('\t"Clients per user-agent" map reduce took %d ms', stats.processtime) 
+                done();
+            }
+        });
+
+        RawData.mapReduce(UserSessionDurationMapReduce, (err, data, stats) => { 
+            if (err)
+                reject(err)
+            else if (stats){
+                console.log('\t"Duration per device" map reduce took %d ms', stats.processtime)
+                done();
+            }
+        });
+
+        // function called in map reduce callbacks
+        function done() {
+            // one map reduced finished
+            done.counter++;
+            // when all aggragations done
+            if (done.counter > 2)
+                resolve('aggregations done')
+        }
+        // initialize counter static variable
+        done.counter = 0;
+        // status_ok ? resolve('aggregation done') : reject('aggregation rejected')        
+    });
+};
+
+const readSessionPromise = (req) => {
     return new Promise( (resolve, reject) => {
         fs.readFile(contextSessionFile.path, (err, data) => {
             if (err) {
                 reject(err)
             }
             else {
-                const dictionary = res.dictionary;
-                const RawData = res.model;
+                const dictionary = req.dictionary;
+                const RawData = req.model;
+                const rawDataJson = {}
+                let lines_ok = 0;
+
                 data = data.toString();
                 lines = data.split('\n');
-                const rawDataJson = {}
                 lines.map( (line) => {
                     var fields = line.split('|');
                     for (var i=0; i< dictionary.length; i++){
@@ -77,16 +223,22 @@ const readSessionPromise = (res) => {
                             if (err){
                                 console.log('err '+err);
                             }
+                            else
+                                lines_ok++;
                         });
                     }
                 });
-                resolve('session data');
 
+                const res = {
+                    model: req.model,                    
+                    previousPhase: 'Session data addition',
+                    lines_ok: lines_ok
+                }
+                resolve(res);
             }
         });
     });
 };
-
 
 const readDictionaryPromise = () => {
     return new Promise( (resolve, reject) => {
@@ -96,7 +248,6 @@ const readDictionaryPromise = () => {
             }
             else {
                 data = data.toString();
-                console.log("read dictionary");
                 const dictionary = data.split('|');
                 const rawDataSchemaJson = {};
                 dictionary.map( (word) => {
@@ -121,30 +272,28 @@ const readDictionaryPromise = () => {
                 const RawData = mongoose.model("RawData", rawDataSchema);
                 const res = {
                     model: RawData,
-                    dictionary: dictionary
+                    dictionary: dictionary,
+                    previousPhase: 'Dictionary addition'
                 }
                 resolve(res);                
-                // saveData.call(contextNodeJsEventLoopFile, data, resolve, reject);
             }
         });
     })
 }
 
-
-
 function saveData(data, resolve, reject)  {
-    // this bind from outside
-    console.log('saveData '+ this.id)
+    // this bound from outside
+    console.log('\tSaving data file '+ this.id)
     const f = new FileData();
     f.value = data;
     f._id = this.id;
     f.save( (err) => {
         if (err){
-            console.log('ERR with '+f.id+' : '+err)
+            console.log('\tERR with '+f.id+' : '+err)
             reject(err);        
         }
         else{
-            console.log('SUCCESS - '+f.id+' added');
+            console.log('\tSUCCESS - '+f.id+' added');
             resolve(this.id);
         }
     });
