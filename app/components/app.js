1e90a226673c05a3e2fe274dd5a705af48f39026
noshir <noshir@localhost.localdomain>
2017-01-04 10:11:35 +0200

    fixed typo

--- app/components/app.js
+++ app/components/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from './main/Header';
 import Footer from './main/Footer';
-/
+
 export default class App extends React.Component {
     static get childContextTypes() {
         return {
